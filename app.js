697d62a979c03f3be481fc171344fcb3e9ceb009
unni <unni@bloomboard.com>
2020-04-23 00:47:14 +0530

    verified graphiql interface

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
+const app = express();
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-    res.send('Hello World');
-});
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Combo', 'Fixed', 'Platter'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    }
+}));
 
 app.listen(3000);

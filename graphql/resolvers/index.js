c704c7f1f24672e7eddfad3dee610a8794ac93f2
unni <unni@bloomboard.com>
2020-04-27 23:24:56 +0530

    added async operation

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,34 +3,30 @@ const Event = require('../../models/event');
 const User = require('../../models/user');
 const Booking = require('../../models/booking');
 
-const events = (eventIds) => {
-    return Event.find({ _id: { $in: eventIds } })
-        .then(events => {
-            return events.map(event => {
-                return {
-                    ...event._doc,
-                    date: new Date(event._doc.date).toISOString(),
-                    creator: user.bind(this, event.creator)
-                }
-            });
-        })
-        .catch(err => {
-            throw err;
+const events = async (eventIds) => {
+    try {
+        const events = await Event.find({ _id: { $in: eventIds } });
+        return events.map(event => {
+            return {
+                ...event._doc,
+                date: new Date(event._doc.date).toISOString(),
+                creator: user.bind(this, event.creator)
+            }
         });
+    }
+    catch (err) { throw err; }
 }
 
-const user = (userId) => {
-    return User.findById(userId)
-        .then(user => {
-            return {
-                ...user._doc,
-                password: null,
-                createdEvents: events.bind(this, user._doc.createdEvents)
-            };
-        })
-        .catch((err) => {
-            throw err
-        });
+const user = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        return {
+            ...user._doc,
+            password: null,
+            createdEvents: events.bind(this, user._doc.createdEvents)
+        };
+    }
+    catch (err) { throw err; }
 }
 const singleEvent = async (eventId) => {
     try {
@@ -40,29 +36,26 @@ const singleEvent = async (eventId) => {
             creator: user.bind(this, event._doc.creator)
         }
     }
-    catch{
-        err => {
-            throw err;
-        }
+    catch (err) {
+        throw err;
     }
 }
 
 module.exports = {
-    events: () => {
-        return Event.find()
-            .then(events => {
-                return events.map(event => {
-                    return {
-                        ...event._doc,
-                        date: new Date(event._doc.date).toISOString(),
-                        creator: user.bind(this, event._doc.creator) //store user instead
-                    }
-                })
-            })
-            .catch(err => {
-                console.log(err);
-                throw err;
+    events: async () => {
+        try {
+            const events = await Event.find();
+            return events.map(event => {
+                console.log(event);
+                return {
+                    ...event._doc,
+                    date: new Date(event._doc.date).toISOString(),
+                    creator: user.bind(this, event._doc.creator)
+                }
             });
+
+        }
+        catch (err) { throw err; }
     },
     bookings: async () => {
         try {
@@ -84,75 +77,72 @@ module.exports = {
             }
         }
     },
-    createEvent: (args) => {
-        const event = new Event({
-            title: args.eventInput.title,
-            description: args.eventInput.description,
-            price: args.eventInput.price,
-            date: args.eventInput.date,
-            creator: '5ea34470af68fc328e934a22'
-        });
-
-        let createdEvents;
-
-        return event.save()
-            .then(result => {
-                createdEvents = {
-                    ...result._doc,
-                    date: new Date(event._doc.date).toISOString(),
-                    creator: user.bind(this, result.creator)
-                };
-                return User.findById('5ea34470af68fc328e934a22');
-            })
-            .then(user => {
-                if (!user) {
-                    throw new Error('User does not exist');
-                }
-                user.createdEvents.push(event);
-                return user.save();
-            })
-            .then(result => {
-                return createdEvents;
-            })
-            .catch(err => {
-                console.log(err);
-                throw err;
+    createEvent: async (args) => {
+        try {
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date,
+                creator: '5ea70861c26caa1de0a9c7c0'
             });
+
+            //save the event
+            let savedEvent;
+            const result = await event.save();
+            savedEvent = {
+                ...result._doc,
+                date: new Date(result._doc.date).toISOString(),
+                creator: user.bind(this, result._doc.creator)
+            };
+
+            //update user Model
+            const creator = await User.findById('5ea70861c26caa1de0a9c7c0');
+            if (!creator) {
+                throw new Error('User does not exist');
+            }
+
+            creator.createdEvents.push(event);
+            await creator.save();
+
+            return savedEvent;
+        }
+        catch (err) { throw err; }
     },
-    createUser: (args) => {
-        return User.findOne({ email: args.userInput.email })
-            .then(user => {
-                if (user) {
-                    throw new Error('User exists already');
-                }
-                return bcrypt.hash(args.userInput.password, 12);
-            })
-            .then(hashedPassword => {
-                return user = new User({
-                    email: args.userInput.email,
-                    password: hashedPassword
-                });
-            })
-            .then(user => {
-                return user.save();
-            })
-            .then(result => {
-                return {
-                    ...result._doc,
-                    password: null
-                };
-            })
-            .catch(err => {
-                console.log(err);
-                throw err;
+    createUser: async args => {
+        try {
+
+            const existingUser = await User.findOne({ email: args.userInput.email });
+
+            if (existingUser) {
+                console.log(existingUser);
+                throw new Error('User exists already.');
+            }
+
+            const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+
+            const user = new User({
+                email: args.userInput.email,
+                password: hashedPassword
             });
+
+            const result = await user.save();
+
+            return {
+                ...result._doc,
+                password: null
+            };
+        }
+        catch (err) {
+            throw err;
+        }
     },
     bookEvent: async (args) => {
         try {
             const fetchedEvent = await Event.findOne({ _id: args.eventId });
             const booking = new Booking({
                 event: fetchedEvent,
-                user: '5ea34470af68fc328e934a22'
+                user: '5ea70861c26caa1de0a9c7c0'
             });
             const result = await booking.save();
             return {
@@ -168,21 +158,22 @@ module.exports = {
                 throw err;
             }
         }
-
     },
     cancelBooking: async (args) => {
-        try{
+        try {
             const booking = await Booking.findById(args.bookingId).populate('event');
             const event = {
                 ...booking.event._doc,
-                creator: user.bind(this,booking.event._doc.creator)
+                creator: user.bind(this, booking.event._doc.creator)
             }
-            await Booking.deleteOne({_id: args.bookingId});
+            await Booking.deleteOne({ _id: args.bookingId });
             return event;
         }
-        catch{ err => {
-            throw err;
-        }}
+        catch{
+            err => {
+                throw err;
+            }
+        }
 
     }
 }

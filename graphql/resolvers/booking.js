6a50e9f101d462f7ac04e3f04686320fff893801
unni <unni@bloomboard.com>
2020-04-28 01:08:19 +0530

    refactored

---
+++ graphql/resolvers/booking.js
@@ -0,0 +1,36 @@
+const Event = require('../../models/event');
+const Booking = require('../../models/booking');
+const { tranformBooking, transformEvent } = require('./merge');
+
+module.exports = {
+    bookings: async () => {
+        try {
+            const bookings = await Booking.find();
+            return bookings.map(booking => {
+                return tranformBooking(booking);
+            });
+        }
+        catch (err) { throw err; }
+    },
+    bookEvent: async (args) => {
+        try {
+            const fetchedEvent = await Event.findOne({ _id: args.eventId });
+            const booking = new Booking({
+                event: fetchedEvent,
+                user: '5ea70861c26caa1de0a9c7c0'
+            });
+            const result = await booking.save();
+            return tranformBooking(result);
+        }
+        catch (err) { throw err; }
+    },
+    cancelBooking: async (args) => {
+        try {
+            const booking = await Booking.findById(args.bookingId).populate('event');
+            const event = transformEvent(booking.event);
+            await Booking.deleteOne({ _id: args.bookingId });
+            return event;
+        }
+        catch (err) { throw err; }
+    }
+}
---
+++ graphql/resolvers/events.js
@@ -0,0 +1,43 @@
+const Event = require('../../models/event');
+const User = require('../../models/user');
+const { transformEvent } = require('./merge');
+
+module.exports = {
+    events: async () => {
+        try {
+            const events = await Event.find();
+            return events.map(event => {
+                return transformEvent(event);
+            });
+        }
+        catch (err) { throw err; }
+    },
+    createEvent: async (args) => {
+        try {
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date,
+                creator: '5ea70861c26caa1de0a9c7c0'
+            });
+
+            //save the event
+            let savedEvent;
+            const result = await event.save();
+            savedEvent = transformEvent(result);
+
+            //update user Model
+            const creator = await User.findById('5ea70861c26caa1de0a9c7c0');
+            if (!creator) {
+                throw new Error('User does not exist');
+            }
+
+            creator.createdEvents.push(event);
+            await creator.save();
+
+            return savedEvent;
+        }
+        catch (err) { throw err; }
+    }
+}

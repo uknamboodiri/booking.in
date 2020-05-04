62f7d3e679bfd445a4c3d775d89c7612bb433d05
Unnikrishnan <uknamboodiri@gmail.com>
2020-05-04 12:01:26 +0530

    updated message

--- frontend/src/pages/bookings.js
+++ frontend/src/pages/bookings.js
@@ -2,7 +2,7 @@ import React, { Component} from 'react';
 
 class BookingsPage extends Component{
     render(){
-        return (<h1>The Bookings Page</h1>);
+        return (<h1>The Booking Page</h1>);
     };
 
 }
